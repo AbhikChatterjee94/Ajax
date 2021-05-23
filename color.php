001f68c91a090727505e2bfe9c1c934f94629a78
Abhik <chatterjeeabhik94@gmail.com>
2021-05-23 19:19:17 +0530

    Commit

---
+++ color.php
@@ -0,0 +1,36 @@
+<label>Color Name </label>
+<select id="color_id" onchange="get_color()">
+    <option value="">Choose Color</option>
+    <?php
+    $sql = mysqli_query($conn, "SELECT * FROM color");
+    while($colors = mysqli_fetch_assoc($sql)){ 
+    ?>
+    <option value="<?=$colors['clr_id']?>"> <?=$colors['clr_name']?> </option>
+    <?php 
+	}
+    ?>
+</select>
+
+<label>Color Display</label>
+<input type="text" id="hex_code" readonly>
+
+<script>
+function get_color(){
+ 	var color_id=$('#color_id').val();
+ 	$.ajax({
+	    url:"api/getcolor.php",
+	    type:"POST",
+		data:'color_id='+color_id,
+	    success:function(data){
+            $("#hex_code").css("background-color",data);
+	    }
+	})
+}
+</script>
+
+<?php
+$color_id = $_REQUEST['color_id'];
+$sql = mysqli_query($conn,"SELECT * FROM color WHERE clr_id='$color_id'");
+$sql_fetch = mysqli_fetch_assoc($sql);
+echo $sql_fetch['clr_name'];
+?>
